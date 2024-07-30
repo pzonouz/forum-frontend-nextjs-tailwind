3e47520f5c25e63347bce1d5e8bdf6d0e14bf890
peyman <p.zonnouz@gmail.com>
2024-07-30 11:22:23 +0330

    cache:no-store on filesSearch

--- app/files/page.js
+++ app/files/page.js
@@ -1,7 +1,9 @@
 import FilesSearch from "../components/FilesSearch";
 
 const FilesPage = async () => {
-  const res = await fetch(`${process.env.BACKEND_URL}/files/collection/`);
+  const res = await fetch(`${process.env.BACKEND_URL}/files/collection/`, {
+    cache: "no-store",
+  });
   const files = await res.json();
 
   return <FilesSearch files={files} />;

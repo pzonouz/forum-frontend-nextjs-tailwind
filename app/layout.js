ce79242bfaa9ff0183f600a926d07dbd5c069527
peyman <p.zonnouz@gmail.com>
2024-07-31 14:57:48 +0330

    fix fetch on filed search

--- app/layout.js
+++ app/layout.js
@@ -53,13 +53,10 @@ export const metadata = {
   title: "انجمن تخصصی برق خودرو",
   description: "برق خودرو،سیم کشی،ایسیو،ECU،تست موتور،عیب یابی،دیاگ",
 };
-let files = [];
-try {
-  const res = await fetch(`${process.env.BACKEND_URL}/files/collection/`, {
-    cache: "no-store",
-  });
-  files = await res?.json();
-} catch (error) {}
+const res = await fetch(`${process.env.BACKEND_URL}/files/collection/`, {
+  cache: "no-store",
+});
+const files = await res?.json();
 
 export default function RootLayout({ children }) {
   return (

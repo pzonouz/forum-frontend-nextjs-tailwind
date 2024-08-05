cfe636ef7ff7b55662ca15808540a3fc12b271b9
peyman <p.zonnouz@gmail.com>
2024-08-05 16:51:02 +0330

    add page advertise to files download page

--- app/files/[id]/page.js
+++ app/files/[id]/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const FilePage = async ({ params }) => {
   const { id } = params;
   let file = {};
@@ -17,6 +19,18 @@ const FilePage = async ({ params }) => {
         >
           دانلود
         </a>
+
+        <div className="flex flex-col items-center">
+          <div className="mt-4 font-semibold text-lg text-pretty text-center ">
+            تخصصی ترین انجمن برق خودرو کاملا رایگان در ایران
+          </div>
+          <div className="text-pretty text-center mt-4 text-gray-700">
+            دسترسی به انبوهی از پرسش ها و پاسخ های مربوطه در زمینه برق خودرو
+          </div>
+          <Link href={"/"} className="mt-4 text-center text-primary">
+            برای مشاهده آخرین سوالات کلیک کنید
+          </Link>
+        </div>
       </div>
     </div>
   );
